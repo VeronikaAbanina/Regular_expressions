a4ddc2eebfd7693bea087873e21f208b1e3763de
Veronika Abanina <220390@mail.ru>
2022-10-07 14:32:18 +0300

    разобралась с дублями

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
-with open("phonebook_raw.csv") as f:
+with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
 #pprint(contacts_list)
@@ -10,8 +10,8 @@ with open("phonebook_raw.csv") as f:
 # TODO 1: выполните пункты 1-3 ДЗ
 import re
 
-pattern_phone = r'(\+7|8)?\s*\(?(\d{3})\)?\s*?\-?(\d{3})[-\s]?(\d{2})[-\s]?(\d+)\s?(\(?\w{3}\.\)?\s*?\d{4})?'
-substitution_phone = r'+7(\2)\3-\4-\5 \6'
+pattern_phone = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
+substitution_phone = r'+7(\2)-\3-\4-\5 \6\7'
 
 contacts_list_new = list()
 for page in contacts_list:
@@ -22,7 +22,7 @@ for page in contacts_list:
 #pprint(contacts_list_new)
 
 
-pattern_name = r'(^[А-ЯЁа-яё]+)\s*\,?([А-ЯЁа-яё]+)\s*\,?([А-ЯЁа-яё]*)\,{1,10}'
+pattern_name = r'(^[А-ЯЁа-яё]+)\s*\,?([А-ЯЁа-яё]+)\s*\,?([А-ЯЁа-яё]*)\,{1}'
 substitution_name = r'\1,\2,\3, '
 
 contacts_list_new2 = list()
@@ -34,15 +34,17 @@ for page in contacts_list_new:
 #pprint(contacts_list_new2)
 
 
-# 3) объединить все дублирующиеся записи о человеке в одну.
-phone_dict = {x[0]: x[1:] for x in contacts_list_new2}
+phone_dict = {}
+for x in contacts_list_new2:
+  key = f"{x[0]} {x[1]}"
+  if not phone_dict.get(key):
+    phone_dict[key] = x
+  else:
+    phone_dict[key] = [i[0] if i[0] else i[1] for i in zip(phone_dict[key], x)]
 pprint(phone_dict)
 
 
-
 # TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
 with open("phonebook.csv", "w") as f:
   datawriter = csv.writer(f, delimiter=',')
-  # Вместо contacts_list подставьте свой список
-  datawriter.writerows(contacts_list)
+  datawriter.writerows(phone_dict)
